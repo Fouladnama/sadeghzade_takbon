39b2fcd6fb720e37f9a6c529c3b699d97c5e7953
zahra <zahra@sadegh.com>
2025-07-23 12:33:32 +0330

    Stop tracking files ignored by gitignore

--- src/app/Admin/login/layout.js
+++
@@ -1,7 +0,0 @@
-export default function AdminLoginLayout({ children }) {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-900">
-      {children}
-    </div>
-  );
-}
