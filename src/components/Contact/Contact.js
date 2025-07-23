9225f85cf666c6f9e97adc3f30baf003ca96c64f
zahra <zahra@sadegh.com>
2025-07-23 13:24:13 +0330

    image

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,7 +1,7 @@
 "use client";
 import { useState, useEffect } from "react";
 import { useSearchParams } from 'next/navigation';
-import Navbar from "../../app/Navbar/Navbar.js";
+import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
 import logoGif from "../../../public/Assests/Landing/takbon.gif";
 import contact from "../../../public/Assests/Contact/contact.jpg";
