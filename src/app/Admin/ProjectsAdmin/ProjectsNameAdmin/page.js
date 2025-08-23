c121fbec65e4502c72bd682d41484a190f32ca01
zahra <zahra@sadegh.com>
2025-08-23 08:23:57 +0330

    image

--- src/app/Admin/ProjectsAdmin/ProjectsNameAdmin/page.js
+++ src/app/Admin/ProjectsAdmin/ProjectsNameAdmin/page.js
@@ -1,63 +1,26 @@
 "use client";
-
 import React, { useState, useEffect } from "react";
-import ManagerProject from "../../../../components/ManagerProject/ManagerProject";
-import ApiConfig from "../../../../Api";
+import MangerAdmin from "../../MngerAdmin/MangerAdmin";
+import { Select, MenuItem, FormControl, InputLabel, Box, Tooltip } from "@mui/material";
 
-export default function ProjectsNameAdmin() {
-  const [project, setProject] = useState([]);
+export default function get_equipment() {
 
-  useEffect(() => {
-    ApiConfig
-      .get("https://takbon.biz:3402/get_projects")
-      .then((res) => {
-        if (res.data && Array.isArray(res.data.value)) {
-          setProject(
-            res.data.value.map((project) => ({
-              code: project._id,
-              name: project.fa_name,
-            }))
-          );
-        } else {
-          console.error("فرمت دیتا معتبر نیست:", res.data);
-        }
-      })
-      .catch((err) => {
-        console.error("خطا در دریافت API:", err);
-      });
-  }, []);
-
-  const getProject = (projectNo) => {
-    const found = project.find((i) => i.code === projectNo);
-    return found ? found.name : "نامشخص";
-  };
-
+  const apiUrl = "get_all_projects_name";
   return (
-    <ManagerProject
-      url="https://takbon.biz:3402/get_all_projects_name"
-      columns={[
-        { field: "fa_name", header: "عنوان پروژه" },
-        { field: "image", header: "عکس", useImagesPath: true },
 
+    <MangerAdmin
+      title="انواع پروژه"
+      cart={[
+        { value: "fa_name", header: "نام فارسی" },
+        { value: "en_name", header: "نام انگلیسی" },
         {
-          options: project, // این خط مهمه
-
-          field: "external_link",
-          header: "نوع پروژه",
-          render: (item) => {
-            if (!item.external_link) return "نامشخص";
-
-            const projectArray = Array.isArray(item.external_link)
-              ? item.external_link
-              : [item.external_link];
-
-            return projectArray.map((id, idx) => (
-              <div key={idx}>{getProject(id)}</div>
-            ));
-          },
-        },
+          value: "image",
+          header: " عکس",
+          isImage: true,
+          useImagesPath: true
+        }
       ]}
-      title="اسم پروژه"
+      apiUrl={apiUrl}
     />
   );
 }
